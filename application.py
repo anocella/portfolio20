be01631223e9a2916e8b5ae337a1720470e76039
shuangchan <shuangchan@berkeley.edu>
2017-04-10 23:51:57 -0400

    Add files via upload

--- application.py
+++ application.py
@@ -1,3 +1,7 @@
+
+#W209 Course Project
+#Portfolio 2.0
+
 import sqlite3
 import dateutil.parser as dateparser
 import io
@@ -8,26 +12,27 @@ from flask_cors import CORS
 from scipy.stats.stats import pearsonr
 
 app = Flask(__name__, static_url_path='')
+
 api = Api(app)
 CORS(app)
 
 class HelloWorld(Resource):
     def get(self):
         return {'hello': 'world'}
-    
+
 class NoData(Exception):
     pass
-    
+
 class CorrelationAPI(Resource):
     def get(self):
-        
+
         dp = lambda x: dateparser.parse(x).strftime('%Y-%m-%d')
-        
+
         parser = reqparse.RequestParser()
         parser.add_argument('startDate', type=str)
         parser.add_argument('endDate', type=str)
         args = parser.parse_args()
-        
+
         conn = sqlite3.connect('data/data.db')
         c = conn.cursor()
 
@@ -59,9 +64,17 @@ class CorrelationAPI(Resource):
         for x in range(len(corr)):
             writer.writerow(tuple([assetNames[x]]) + corr[x])
         return Response(dest.getvalue(), mimetype="text")
-    
+
+
 api.add_resource(HelloWorld, '/hello')
 api.add_resource(CorrelationAPI, '/corr')
+
+@app.route('/api/get_optimized_returns', methods=['GET'])
+def get_optimized_returns():
+    returns = open('data/data.tsv', 'r')
+    str_buf = returns.read()
+    returns.close()
+    return str_buf
 
 @app.route('/')
 @app.route('/index.html')
@@ -93,4 +106,4 @@ def send_data(path):
     return send_from_directory('data', path)
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run()
