6be0528de9387e21613851c8fa6f7574d82a8dd1
msaedi <msaedi@sfu.ca>
2017-04-17 23:39:37 -0400

    Using Highstock

--- data/perf/performance.js
+++ data/perf/performance.js
@@ -11,16 +11,13 @@ function perfCharts(port1, port2, dataUrl)
         function(error, dataset)
         {
           if (error) throw err;
-          console.log(dataset)
-          console.log('Hello Im here')
           document.getElementById("nav").hidden = false;
           document.getElementById("drawdown").hidden = false;
           document.getElementById("volatility").hidden = false;
           document.getElementById("sharpe").hidden = false;
           document.getElementById("loading").hidden = true;
 
-          var date = [],
-                dd1 = [],
+          var   dd1 = [],
                 vol1 = [],
                 nav1 = [],
                 sr1 = [],
@@ -31,209 +28,128 @@ function perfCharts(port1, port2, dataUrl)
                 name1 = port1;
                 name2 = port2;
           parseDate = d3.time.format("%Y-%m-%d %H:%M:%S").parse
-          formatDate = d3.time.format("%Y")
+		  formatDate = d3.time.format("%Y-%m-%d")
 
           dataset.map(function(d){
-              date.push(formatDate(parseDate(d.Date)));
-              dd1.push(+d.port1_Drawdown);
-              vol1.push(+d.port1_Volatility);
-              nav1.push(+d.port1_NAV);
-              sr1.push(+d.port1_Sharpe);
-              dd2.push(+d.port2_Drawdown);
-              vol2.push(+d.port2_Volatility);
-              nav2.push(+d.port2_NAV);
-              sr2.push(+d.port2_Sharpe);
+              nav1.push([new Date(parseDate(d.Date)).getTime(),+d.port1_NAV])
+			  nav2.push([new Date(parseDate(d.Date)).getTime(),+d.port2_NAV])
+
+			  dd1.push([new Date(parseDate(d.Date)).getTime(),+d.port1_Drawdown])
+			  dd2.push([new Date(parseDate(d.Date)).getTime(),+d.port2_Drawdown])
+
+			  vol1.push([new Date(parseDate(d.Date)).getTime(),+d.port1_Volatility])
+			  vol2.push([new Date(parseDate(d.Date)).getTime(),+d.port2_Volatility])
+
+			  sr1.push([new Date(parseDate(d.Date)).getTime(),+d.port1_Sharpe])
+			  sr2.push([new Date(parseDate(d.Date)).getTime(),+d.port2_Sharpe])
           });
-            console.log(date)
-          Highcharts.chart('nav', {
-            chart: {
-                type: 'line'
-            },
-            title: {
-                text: 'Cumulative return'
-            },
-            subtitle: {
-                text: 'Performance comparison'
-            },
-            xAxis: {
-                categories: date,
-                type: 'datetime',
-                dateTimeLabelFormats: { // don't display the dummy year
-                    day: '%Y',
-                    month: '%Y',
-                    year: '%Y'
-                },
-                title: {
-                    text: 'Date'
-                }
-            },
-            yAxis: {
-                labels: {
-                    formatter: function () {
-                        return '$' + this.value;
-                    }
-                },
-            },
-            plotOptions: {
-                line: {
-                    dataLabels: {
-                        enabled: false
-                    },
-                    enableMouseTracking: true
-                }
-            },
-            series: [{
-                name: name1,
-                data: nav1
-            }, {
-                name: name2,
-                data: nav2
-            }]
-        }); // end of nav
-          Highcharts.chart('drawdown', {
-            chart: {
-                type: 'line'
-            },
-            title: {
-                text: 'Drawdown'
-            },
-            subtitle: {
-                text: 'Performance comparison'
-            },
-            xAxis: {
-                categories: date,
-                type: 'datetime',
-                dateTimeLabelFormats: { // don't display the dummy year
-                    day: '%Y',
-                    month: '%Y',
-                    year: '%Y'
-                },
-                title: {
-                    text: 'Date'
-                }
-            },
-            yAxis: {
-                title: {
-                    text: 'Drawdown'
-                },
-                labels: {
-                    formatter: function () {
-                        return (this.value > 0 ? ' + ' : '') + this.value*100 + '%';
-                    }
-                },
-            },
-            plotOptions: {
-                line: {
-                    dataLabels: {
-                        enabled: false
-                    },
-                    enableMouseTracking: true
-                }
-            },
-            series: [{
-                name: name1,
-                data: dd1
-            }, {
-                name: name2,
-                data: dd2
-            }]
-        }); // end of drawdown
-          Highcharts.chart('volatility', {
-            chart: {
-                type: 'line'
-            },
-            title: {
-                text: '12-month rolling volatility'
-            },
-            subtitle: {
-                text: 'Performance comparison'
-            },
-            xAxis: {
-                categories: date,
-                type: 'datetime',
-                dateTimeLabelFormats: { // don't display the dummy year
-                    day: '%Y',
-                    month: '%Y',
-                    year: '%Y'
-                },
-                title: {
-                    text: 'Date'
-                }
-            },
-            yAxis: {
-                title: {
-                    text: 'Volatility'
-                },
-                labels: {
-                    formatter: function () {
-                        return this.value*100 + '%';
-                    }
-                },
-            },
-            plotOptions: {
-                line: {
-                    dataLabels: {
-                        enabled: false
-                    },
-                    enableMouseTracking: true
-                }
-            },
-            series: [{
-                name: name1,
-                data: vol1
-            }, {
-                name: name2,
-                data: vol2
-            }]
-        }); // end of volatility
-          Highcharts.chart('sharpe', {
-            chart: {
-                type: 'line'
-            },
-            title: {
-                text: 'Sharpe ratio'
-            },
-            subtitle: {
-                text: 'Performance comparison'
-            },
-            xAxis: {
-                categories: date,
-                type: 'datetime',
-                dateTimeLabelFormats: { // don't display the dummy year
-                    day: '%Y',
-                    month: '%Y',
-                    year: '%Y'
-                },
-                title: {
-                    text: 'Date'
-                }
-            },
-            yAxis: {
-                title: {
-                    text: 'Sharpe ratio'
-                },
-                labels: {
-                    formatter: function () {
-                        return (this.value > 0 ? ' + ' : '') + this.value;
-                    }
-                },
-            },
-            plotOptions: {
-                line: {
-                    dataLabels: {
-                        enabled: false
-                    },
-                    enableMouseTracking: true
-                }
-            },
-            series: [{
-                name:name1,
-                data: sr1
-            }, {
-                name: name2,
-                data: sr2
-            }]
-        }); // end of Sharpe ratio
+			Highcharts.stockChart('nav', {
+				rangeSelector: {
+					selected: 1
+				},
+
+				title: {
+					text: 'Growth of $100'
+				},
+				xAxis: {
+					type: 'datetime'
+						},
+				series: [{
+					name: name1,
+					data: nav1,
+					tooltip: {
+						valueDecimals: 2
+					}
+				},
+				{
+					name: name2,
+					data: nav2,
+					tooltip: {
+						valueDecimals: 2
+					}
+				}]
+			}); // end of nav
+
+			Highcharts.stockChart('drawdown', {
+				rangeSelector: {
+					selected: 1
+				},
+
+				title: {
+					text: 'Drawdown'
+				},
+				xAxis: {
+					type: 'datetime'
+						},
+				series: [{
+					name: name1,
+					data: dd1,
+					tooltip: {
+						valueDecimals: 2
+					}
+				},
+				{
+					name: name2,
+					data: dd2,
+					tooltip: {
+						valueDecimals: 2
+					}
+				}]
+			}); // end of drawdown
+
+			Highcharts.stockChart('volatility', {
+				rangeSelector: {
+					selected: 1
+				},
+
+				title: {
+					text: 'Portfolio Volatility'
+				},
+				xAxis: {
+					type: 'datetime'
+						},
+				series: [{
+					name: name1,
+					data: vol1,
+					tooltip: {
+						valueDecimals: 2
+					}
+				},
+				{
+					name: name2,
+					data: vol2,
+					tooltip: {
+						valueDecimals: 2
+					}
+				}]
+			}); // end of volatility
+
+			Highcharts.stockChart('sharpe', {
+				rangeSelector: {
+					selected: 1
+				},
+
+				title: {
+					text: 'Sharpe Ratio'
+				},
+				xAxis: {
+					type: 'datetime'
+						},
+				series: [{
+					name: name1,
+					data: sr1,
+					tooltip: {
+						valueDecimals: 2
+					}
+				},
+				{
+					name: name2,
+					data: sr2,
+					tooltip: {
+						valueDecimals: 2
+					}
+				}]
+			}); // end of sharpe ratio
         }
     );
 
