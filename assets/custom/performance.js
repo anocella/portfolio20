06eb1253d92007911d5f89ad597c2f46ec79e2d6
msaedi <msaedi@sfu.ca>
2017-04-20 00:40:05 -0400

    Added description for the performance page

--- assets/custom/performance.js
+++ assets/custom/performance.js
@@ -4,6 +4,10 @@ function perfCharts(port1, port2, dataUrl)
     document.getElementById("drawdown").hidden = true;
     document.getElementById("volatility").hidden = true;
     document.getElementById("sharpe").hidden = true;
+    document.getElementById("navpt").hidden = true;
+    document.getElementById("volpt").hidden = true;
+    document.getElementById("ddpt").hidden = true;
+    document.getElementById("srpt").hidden = true;
     document.getElementById("loading").hidden = false;
 
     d3.csv(
@@ -15,6 +19,10 @@ function perfCharts(port1, port2, dataUrl)
           document.getElementById("drawdown").hidden = false;
           document.getElementById("volatility").hidden = false;
           document.getElementById("sharpe").hidden = false;
+          document.getElementById("navpt").hidden = false;
+        document.getElementById("volpt").hidden = false;
+        document.getElementById("ddpt").hidden = false;
+        document.getElementById("srpt").hidden = false;
           document.getElementById("loading").hidden = true;
 
           var   dd1 = [],
@@ -368,4 +376,21 @@ d3.select("#compare").on("click", function() {perfCharts(ans1[ans1.selectedIndex
 												ans2[ans2.selectedIndex].value + "&startDate=" +
 												document.getElementById("sd").value + "&endDate=" +
 												document.getElementById("ed").value)})
+
+d3.select("#navp").append("p").attr({"id": "navpt","class": "text-sm-left"}).text("Decision to invest " +
+    "is made easier if a portfolio delivers long-run returns comparable to riskier options.");
+document.getElementById("navpt").hidden = true;
+d3.select("#volatilityp").append("p").attr({"id": "volpt","class": "text-sm-left"}).text("A low total portfolio " +
+    "volatility discourages the " +
+    "market-timing that hurts many investors.");
+document.getElementById("volpt").hidden = true;
+d3.select("#drawdownp").append("p").attr({"id": "ddpt","class": "text-sm-left"}).text("If the following set of assets " +
+    "is the only portfolio " +
+    "the investors hold, then total peak-to-trough loss is even more important than if it were just one product they " +
+    "hold.");
+document.getElementById("ddpt").hidden = true;
+d3.select("#sharpep").append("p").attr({"id": "srpt","class": "text-sm-left"}).text("If the following set of assets is " +
+    "the only portfolio " +
+    "the investors hold, then the most important thing is a good total risk-adjusted return.");
+document.getElementById("srpt").hidden = true;
 //perfCharts("/perf?portName=myport&startDate=9/01/2007&endDate=01/02/2014")
